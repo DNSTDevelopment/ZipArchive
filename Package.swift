9736e3d1bfdeb6a89a53675efa7e201dfad59988
DNSTDevelopment <138738804+DNSTDevelopment@users.noreply.github.com>
2024-01-19 09:26:05 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -19,7 +19,6 @@ let package = Package(
         .target(
             name: "ZipArchive",
             path: "SSZipArchive",
-            swiftSettings: [.unsafeFlags(["-enable-library-evolution","-emit-module-interface"])],
             resources: [
                 .process("Supporting Files/PrivacyInfo.xcprivacy")],
             cSettings: [
@@ -32,6 +31,7 @@ let package = Package(
                 .define("ZLIB_COMPAT")
             ],
             linkerSettings: [
+                .unsafeFlags(["-enable-library-evolution","-emit-module-interface"]),
                 .linkedLibrary("z"),
                 .linkedLibrary("iconv"),
                 .linkedFramework("Security"),
